6c9c7d651332f611d0627236d4c53d5903b664ca
Pdawg11239 <83825746+Pdawg-bytes@users.noreply.github.com>
2022-12-06 18:53:50 -0500

    remove wpfui project, replace with mahmetro

---
+++ WinPEBuilder.WPF/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using MahApps.Metro.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace WinPEBuilder.WPF
+{
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("explorer", "https://github.com/MishaTY/WinPEBuilder/");
+        }
+    }
+}
