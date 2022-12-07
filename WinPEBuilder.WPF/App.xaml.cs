30add57cb618c855f2dbe1897d271e4bad9b34e2
Pdawg11239 <83825746+Pdawg-bytes@users.noreply.github.com>
2022-12-06 21:35:13 -0500

    Lots of work on WPF port

--- WinPEBuilder.WPF/App.xaml.cs
+++ WinPEBuilder.WPF/App.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using ControlzEx.Theming;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using ControlzEx.Theming;
+using System.Windows.Media;
 
 namespace WinPEBuilder.WPF
 {
@@ -13,5 +16,12 @@ namespace WinPEBuilder.WPF
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Set the application theme to Dark.Green
+            ThemeManager.Current.ChangeTheme(this, "Dark.Purple");
+        }
     }
 }
--- WinPEBuilder.WPF/MainWindow.xaml.cs
+++ WinPEBuilder.WPF/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
+using MahApps.Metro.Controls.Dialogs;
 
 namespace WinPEBuilder.WPF
 {
@@ -22,11 +23,83 @@ namespace WinPEBuilder.WPF
         public MainWindow()
         {
             InitializeComponent();
+            UWPBox.IsEnabled = false;
+            LogonUIBox.IsEnabled = false;
+            ExplorerBox.IsEnabled = false;
         }
 
         private void GitButton_Click(object sender, RoutedEventArgs e)
         {
             Process.Start("explorer", "https://github.com/MishaTY/WinPEBuilder/");
         }
+
+        private void DWMBox_Click(object sender, RoutedEventArgs e)
+        {
+            if (DWMBox.IsChecked == true)
+            {
+                UWPBox.IsEnabled = true;
+                LogonUIBox.IsEnabled = true;
+                ExplorerBox.IsEnabled = true;
+                // Port backend event
+            }
+            else
+            {
+                UWPBox.IsEnabled = false;
+                LogonUIBox.IsEnabled = false;
+                ExplorerBox.IsEnabled = false;
+                UWPBox.IsChecked = false;
+                LogonUIBox.IsChecked = false;
+                ExplorerBox.IsChecked = false;
+                // Port backend event
+            }
+        }
+
+        private void UWPBox_Click(object sender, RoutedEventArgs e)
+        {
+            if (UWPBox.IsChecked == true)
+            {
+                // Port backend event
+            }
+            else
+            {
+                // Port opposite backend event
+            }
+        }
+
+        private void LogonUIBox_Click(object sender, RoutedEventArgs e)
+        {
+            if (LogonUIBox.IsChecked == true)
+            {
+                // Port backend event
+            }
+            else
+            {
+                // Port opposite backend event
+            }
+        }
+
+        private void ExplorerBox_Click(object sender, RoutedEventArgs e)
+        {
+            if (ExplorerBox.IsChecked == true)
+            {
+                // Port backend event
+            }
+            else
+            {
+                // Port opposite backend event
+            }
+        }
+
+        private void BuildButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Port backend event
+        }
+
+        private async void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Pdawg: I've got the settings, you just work on backend :)
+        }
+
+        // FOR MISHA: Use the "await this.ShowMessageAsync("This is the title", "Some message");" code to show errors. It fits well with the metro theme.
     }
 }
