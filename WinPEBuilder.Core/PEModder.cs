344e80d98e65d64a0f32c24052649f1180460539
Misha <46088515+MishaTY@users.noreply.github.com>
2023-02-20 19:14:53 -0500

    Update PEModder.cs

--- WinPEBuilder.Core/PEModder.cs
+++ WinPEBuilder.Core/PEModder.cs
@@ -238,7 +238,7 @@ namespace WinPEBuilder.Core
 
         internal bool Run()
         {
-            Process.Start("dism", "/image:" + Base + " /Set-TargetPath:X:").WaitForExit();
+            Process.Start("dism", "/image:" + Base + " /Set-TargetPath:X:\\").WaitForExit();
             //needed for modern explorer
             CopyFile("Windows/System32/shellstyle.dll");
             Directory.CreateDirectory(Base + "Windows/Resources/Themes/Aero/Shell/");
